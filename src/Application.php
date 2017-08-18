5caf32252fe550d66cfff753100270ffad62a760
Elf Sundae <elf.sundae@gmail.com>
2017-08-18 16:57:59 +0800

    Rewrite Application

--- src/Application.php
+++ src/Application.php
@@ -3,13 +3,12 @@
 namespace ElfSundae\Console;
 
 use Closure;
-use Exception;
 use Illuminate\Container\Container;
 use Symfony\Component\Console\Command\Command;
 use Illuminate\Events\Dispatcher as EventsDispatcher;
-use Illuminate\Console\Application as LaravelConsoleApplication;
+use Illuminate\Console\Application as LaravelApplication;
 
-class Application extends LaravelConsoleApplication
+class Application extends LaravelApplication
 {
     /**
      * Create a new console application.
@@ -19,7 +18,11 @@ class Application extends LaravelConsoleApplication
      */
     public function __construct($name = 'Console Application', $version = '1.0.0')
     {
-        parent::__construct($laravel = new Container, new EventsDispatcher($laravel), $version);
+        parent::__construct(
+            $laravel = new Container,
+            new EventsDispatcher($laravel),
+            $version
+        );
 
         $this->setName($name);
         $this->setAutoExit(true);
@@ -27,59 +30,17 @@ class Application extends LaravelConsoleApplication
     }
 
     /**
-     * Register a new command.
-     *
-     * @param  mixed  $command
-     * @return $this
-     */
-    public function register($command)
-    {
-        $num = func_num_args();
-
-        if (is_string($command) && $num > 1 && func_get_arg(1) instanceof Closure) {
-            $command = call_user_func_array([static::class, 'makeCommand'], func_get_args());
-        }
-
-        if ($command instanceof Command) {
-            $this->add($command);
-        } else {
-            throw new Exception("Invalid parameters", 1);
-        }
-
-        if ($num > 1 && func_get_arg($num - 1) === true) {
-            $this->setDefaultCommand($command->getName(), true);
-        }
-
-        return $this;
-    }
-
-    /**
-     * Create a new Closure based command.
+     * Register a Closure based command.
      *
      * @param  string  $signature
      * @param  \Closure  $callback
      * @param  string  $description
      * @return \ElfSundae\Console\ClosureCommand
      */
-    public static function makeCommand($signature, Closure $callback, $description = null)
+    public function command($signature, Closure $callback, $description = null)
     {
-        return (new ClosureCommand($signature, $callback))->describe($description);
-    }
-
-    /**
-     * Create a new console application with a single command and run it.
-     *
-     * @param  string  $name
-     * @param  string  $version
-     * @param  mixed  ...$command
-     * @return int
-     */
-    public static function execute($name, $version, ...$command)
-    {
-        $command[] = true;
-
-        return (new static($name, $version))
-            ->register(...$command)
-            ->run();
+        return $this->add(
+            (new ClosureCommand($signature, $callback))->describe($description)
+        );
     }
 }
